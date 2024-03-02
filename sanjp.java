68b3e94713282cf1f891bf5104f8f74446f9c9b3
patilsanjana03 <patilsanjana197@gamil.com>
2024-03-02 21:07:35 +0530

    javafile

---
+++ sanjp.java
@@ -0,0 +1,24 @@
+data type?", new String[] {"int","char","struct","None of the
+above"}," "));
+questions.add(new Question("Which Company developed the
+Python programming language", new String[]
+{"Microsoft","Google","Mozilla","CWI"},""));
+questions.add(new Question("Which of the following is used to
+define a block of code in python language?",new String[]
+{"Indentation","Key","Brackets","All"}," Indentation"));
+questions.add(new Question("Which operator is used to
+concatenate two strings?",new String[] {"+",".","*","&"},""));
+questions.add(new Question("What is the output of 5//2?",new
+String[] {"2.5","2.0","2","3"},"2"));
+questions.add(new Question("Number of primitive data in java
+are: ", new String[] {"6","7","8","9"},"8"));
+questions.add(new Question("Size of float and double in java is:
+",new String[] {"32 and 64","32 and 32","64 and 64","64 and 32"},"32
+and 64"));
+questions.add(new Question("Can we keep different class name
+and java file name?",new String[] {"Yes","No"},"Yes"));
+questions.add(new Question("Can main method be overloaded
+in java?",new String[] {"Yes","No"},"Yes"));
+questions.add(new Question("What are types of memory
+allocated in java?",new String[] {"Heap memory","Stack
+memory","Both A and B","None"},"Both A and B"));
